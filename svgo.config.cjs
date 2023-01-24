49054745bd460c7bc379ad75bae39360b74144c9
Nic Malan <nicmalan@gmail.com>
2023-01-24 15:05:59 +0200

    svgo script to minify the svgs

---
+++ svgo.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  multipass: true,
+  js2svg: {
+    indent: 2,
+    pretty: true,
+  },
+  plugins: [{
+    name: 'preset-default',
+    params: {
+      overrides: {
+        cleanupIds: false,
+      },
+    },
+  }],
+};
